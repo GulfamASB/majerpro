fb666df364db3d02821a9387be10dfbfa1207893
GulfamASB <128397086+GulfamASB@users.noreply.github.com>
2023-07-14 15:47:02 +0530

    Update next.config.js
    Signed-off-by: GulfamASB <128397086+GulfamASB@users.noreply.github.com>

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   experimental: {
     appDir: true,
     serverComponentsExternalPackages: ["mongoose"],
+    basePath: '/github-pages',
   },
   images:{
     domains:['occ-0-3933-116.1.nflxso.net']
